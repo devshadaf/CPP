55343087b29662f17dd627f62fbe3fbeee5e7ac3
Shadaf Hossain <shadafhossain01@gmail.com>
2026-09-02 21:42:46 +0600

    Foundation 0.1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-  
+  string name;
+  int age;
+  cout << "What is your name:\n"  ;
+  getline(cin,name);
+  cout << "What is your age:\n"  ;
+  cin >> age;
+  cout << "your name is " << name << " Age is "<<age<<" Years old";
   return 0;
 }
